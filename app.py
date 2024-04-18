4e879b088e8cc43afd2d27586034f39c94bb934b
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2024-04-18 15:36:34 +0530

    update backend

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
+from collections import Counter
 from sklearn.feature_extraction.text import CountVectorizer
 
 app = Flask(__name__)
 
-# Load the model and required data
+# Load the saved model and related data
 with open('model.pkl', 'rb') as f:
     knn_model = pickle.load(f)
     cv = pickle.load(f)
+    new_cv = pickle.load(f)
     df_idf = pickle.load(f)
     data = pickle.load(f)
     all_symptoms = pickle.load(f)
     input_symptoms = pickle.load(f)
+    input_age = pickle.load(f)
+    input_gender = pickle.load(f)
     top_diseases = pickle.load(f)
     indices = pickle.load(f)
     distances = pickle.load(f)
@@ -22,6 +26,8 @@ with open('model.pkl', 'rb') as f:
     docs = pickle.load(f)
     word_count_vector = pickle.load(f)
     tfidf_transformer = pickle.load(f)
+    new_word_count_vector = pickle.load(f)
+    new_tfidf_transformer = pickle.load(f)
     input_vector = pickle.load(f)
 
 @app.route('/')
@@ -30,35 +36,56 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get symptoms from request
-    symptoms = request.json.get('symptoms', [])
-    print(symptoms)
-    # Transform input symptoms
-    input_vector = cv.transform(symptoms)
+    # Get data from the request
+    req_data = request.json
+    input_age = req_data['age']
+    input_gender = req_data['gender']
+    input_symptoms = req_data['symptoms']
 
-    # Find nearest neighbors
-    distances, indices = knn_model.kneighbors(input_vector, n_neighbors=300)
-    print(distances)
-    print(indices)
-    # Get top diseases
-    top_diseases = data.iloc[indices[0]]['disease']
-    top_diseases = top_diseases.drop_duplicates()
+    if(len(input_symptoms) < 3):
+        k = 5
+    elif(len(input_symptoms) < 5):
+        k = 3
+    elif(len(input_symptoms) < 7):
+        k = 2
+    else:
+        k = 1
+
+    # Process the data to get the list of symptoms
+    docs = []
+    filter_df = []
+    for i in range(len(df)):
+        item = []
+        for j in range(len(cols)):
+            if df.iloc[i][j] == '1':
+                item.append(cols[j])
+        if(item[-1] == input_age and item[-2] == input_gender):
+            docs.append(','.join(item[:-2]))
+            filter_df.append(data.iloc[i]['disease'])
     
-    # Get top symptoms
+    # Compute the TF-IDF values for input symptoms
+    input_vector = cv.transform(input_symptoms)
+    input_tfidf = tfidf_transformer.transform(input_vector)
+
+    # Find the top k diseases
+    distances, indices = knn_model.kneighbors(input_tfidf, n_neighbors=k)
+
+    top_diseases = []
+    for i in indices[0]:
+        top_diseases.append(filter_df[i])
+
+    # Get top 10 symptoms related to the predicted diseases
     all_symptoms = []
-    for i in indices[-1]:
+    for i in indices[0]:
         symp = docs[i].split(',')
-        symp = symp[: -2]
         all_symptoms += [s for s in symp if s not in input_symptoms]
 
-    all_symptoms = list(set(all_symptoms))
-    all_symptoms = [s for s in all_symptoms if s not in symptoms]
-    all_symptoms.sort(key=lambda x: df_idf.loc[x]['idf_weights'], reverse=True)
-    top_symptoms = all_symptoms[:10]
-
+    counts = Counter(all_symptoms)
+    sorted_symptoms = sorted(counts, key=counts.get)
+    
     return jsonify({
-        'top_diseases': top_diseases.tolist(),
-        'top_symptoms': top_symptoms
+        'top_diseases': top_diseases,
+        'top_symptoms': sorted_symptoms
     })
 
 if __name__ == '__main__':
